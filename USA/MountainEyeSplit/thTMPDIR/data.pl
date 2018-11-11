6e942fca2fa904700a323c0cf247c7801da0e379
rostamnamaghi <42341834+rostamnamaghi@users.noreply.github.com>
2018-11-11 15:53:58 +0000

    Survey Course Update
    Problem areas removed.
    Some minor drawing on several areas.
    Only wall intersect defect in ME3
    Boulder fill needs AA & Wookey file once published.
    Water areas in ME5 will need redoing

---
+++ USA/MountainEyeSplit/thTMPDIR/data.pl
@@ -0,0 +1,321 @@
+%SCRAP = (
+	# scrap: ME22_scrapSP1
+	aaaaaaa => {
+		 F => "data.1",
+		 B => "data.2",
+		 I => "data.2bg",
+		 C => "data.2clip",
+		 E => "data.3",
+		 X => "data.4",
+		 P => "data.4bbox",
+		 Y => 1,
+		 V => -1,
+		 S => "2953.47 -1718.71",
+	},
+	# scrap: ME23_scrapSP1
+	aaaaaab => {
+		 F => "data.5",
+		 B => "data.6",
+		 I => "data.6bg",
+		 C => "data.6clip",
+		 E => "data.7",
+		 X => "data.8",
+		 P => "data.8bbox",
+		 Y => 2,
+		 V => -1,
+		 S => "3059.93 -1667.58",
+	},
+	# scrap: ME21_scrapSP1
+	aaaaaac => {
+		 F => "data.9",
+		 B => "data.10",
+		 I => "data.10bg",
+		 C => "data.10clip",
+		 E => "data.11",
+		 X => "data.12",
+		 P => "data.12bbox",
+		 Y => 3,
+		 V => -1,
+		 S => "2850.07 -1977.69",
+	},
+	# scrap: ME4_scrapSP4
+	aaaaaad => {
+		 B => "data.13",
+		 I => "data.13bg",
+		 C => "data.13clip",
+		 E => "data.14",
+		 X => "data.15",
+		 P => "data.15bbox",
+		 Y => 4,
+		 V => -1,
+		 S => "3714.60 -1193.63",
+	},
+	# scrap: ME4_scrapSP3
+	aaaaaae => {
+		 B => "data.16",
+		 I => "data.16bg",
+		 C => "data.16clip",
+		 E => "data.17",
+		 X => "data.18",
+		 P => "data.18bbox",
+		 Y => 4,
+		 V => -1,
+		 S => "3459.80 -1572.10",
+	},
+	# scrap: ME4_scrapSP2
+	aaaaaaf => {
+		 B => "data.19",
+		 I => "data.19bg",
+		 C => "data.19clip",
+		 E => "data.20",
+		 X => "data.21",
+		 P => "data.21bbox",
+		 Y => 4,
+		 V => -1,
+		 S => "3415.75 -1842.61",
+	},
+	# scrap: ME4_scrapSP1
+	aaaaaag => {
+		 F => "data.22",
+		 B => "data.23",
+		 I => "data.23bg",
+		 C => "data.23clip",
+		 E => "data.24",
+		 X => "data.25",
+		 P => "data.25bbox",
+		 Y => 4,
+		 V => -1,
+		 S => "3964.01 -745.65",
+	},
+	# scrap: ME2_scrapSP1
+	aaaaaah => {
+		 F => "data.26",
+		 B => "data.27",
+		 I => "data.27bg",
+		 C => "data.27clip",
+		 E => "data.28",
+		 X => "data.29",
+		 P => "data.29bbox",
+		 Y => 5,
+		 V => -1,
+		 S => "1413.32 -2789.56",
+	},
+	# scrap: ME1_scrapSP1
+	aaaaaai => {
+		 F => "data.30",
+		 B => "data.31",
+		 I => "data.31bg",
+		 C => "data.31clip",
+		 E => "data.32",
+		 X => "data.33",
+		 P => "data.33bbox",
+		 Y => 6,
+		 V => -1,
+		 S => "527.34 -3124.37",
+	},
+	# scrap: ME3_scrapSP1
+	aaaaaaj => {
+		 F => "data.34",
+		 B => "data.35",
+		 I => "data.35bg",
+		 C => "data.35clip",
+		 E => "data.36",
+		 X => "data.37",
+		 P => "data.37bbox",
+		 Y => 7,
+		 V => -1,
+		 S => "2653.67 -1984.93",
+	},
+	# scrap: ME20_scrapSP1
+	aaaaaba => {
+		 F => "data.38",
+		 B => "data.39",
+		 I => "data.39bg",
+		 C => "data.39clip",
+		 E => "data.40",
+		 X => "data.41",
+		 P => "data.41bbox",
+		 Y => 8,
+		 V => -1,
+		 S => "611.29 -3008.66",
+	},
+	# scrap: ME5_scrapSP2
+	aaaaabb => {
+		 F => "data.42",
+		 B => "data.43",
+		 I => "data.43bg",
+		 C => "data.43clip",
+		 E => "data.44",
+		 X => "data.45",
+		 P => "data.45bbox",
+		 Y => 9,
+		 V => -1,
+		 S => "29.65 -5071.60",
+	},
+	# scrap: ME5_scrapSP1
+	aaaaabc => {
+		 F => "data.46",
+		 B => "data.47",
+		 I => "data.47bg",
+		 C => "data.47clip",
+		 E => "data.48",
+		 X => "data.49",
+		 P => "data.49bbox",
+		 Y => 9,
+		 V => -1,
+		 S => "386.63 -4143.86",
+	},
+	# scrap: bellywalls
+	aaaaabd => {
+		 F => "data.50",
+		 B => "data.51",
+		 I => "data.51bg",
+		 C => "data.51clip",
+		 E => "data.52",
+		 X => "data.53",
+		 P => "data.53bbox",
+		 Y => 10,
+		 V => -1,
+		 S => "4250.71 -517.72",
+	},
+	# scrap: superwalls
+	aaaaabe => {
+		 F => "data.54",
+		 B => "data.55",
+		 I => "data.55bg",
+		 C => "data.55clip",
+		 E => "data.56",
+		 X => "data.57",
+		 P => "data.57bbox",
+		 Y => 10,
+		 V => -1,
+		 S => "4455.84 -303.39",
+	},
+	# scrap: fossiltrunkwalls
+	aaaaabf => {
+		 F => "data.58",
+		 B => "data.59",
+		 I => "data.59bg",
+		 C => "data.59clip",
+		 E => "data.60",
+		 X => "data.61",
+		 P => "data.61bbox",
+		 Y => 10,
+		 V => -1,
+		 S => "4278.36 -250.94",
+	},
+	# scrap: ancientwalls
+	aaaaabg => {
+		 F => "data.62",
+		 B => "data.63",
+		 I => "data.63bg",
+		 C => "data.63clip",
+		 E => "data.64",
+		 X => "data.65",
+		 P => "data.65bbox",
+		 Y => 10,
+		 V => -1,
+		 S => "4524.47 -84.55",
+	},
+	# scrap: Highlinewalls
+	aaaaabh => {
+		 F => "data.66",
+		 B => "data.67",
+		 I => "data.67bg",
+		 C => "data.67clip",
+		 E => "data.68",
+		 X => "data.69",
+		 P => "data.69bbox",
+		 Y => 10,
+		 V => -1,
+		 S => "4843.72 -96.03",
+	},
+	# scrap: cobSP3
+	aaaaabi => {
+		 B => "data.70",
+		 I => "data.70bg",
+		 C => "data.70clip",
+		 E => "data.71",
+		 X => "data.72",
+		 P => "data.72bbox",
+		 Y => 11,
+		 V => -2,
+		 S => "5196.86 -100.54",
+	},
+	# scrap: cobSP2
+	aaaaabj => {
+		 B => "data.73",
+		 I => "data.73bg",
+		 C => "data.73clip",
+		 E => "data.74",
+		 X => "data.75",
+		 P => "data.75bbox",
+		 Y => 11,
+		 V => -1,
+		 S => "5013.03 -148.80",
+	},
+	# scrap: cobSP1
+	aaaaaca => {
+		 B => "data.76",
+		 I => "data.76bg",
+		 C => "data.76clip",
+		 E => "data.77",
+		 X => "data.78",
+		 P => "data.78bbox",
+		 Y => 11,
+		 V => -1,
+		 S => "5158.17 -248.31",
+	},
+);
+
+
+%LAYER = (
+	# expanded map: mountaineye22
+	1 => {
+		N => 'Mountain eye cave part 22',
+	},
+	# expanded map: mountaineye23
+	2 => {
+		N => 'Mountain eye cave part 23',
+	},
+	# expanded map: mountaineye21
+	3 => {
+		N => 'Mountain eye cave part 21',
+	},
+	# expanded map: mountaineye4
+	4 => {
+		N => 'Mountain eye cave part 4',
+	},
+	# expanded map: mountaineye2
+	5 => {
+		N => 'Mountain eye cave part 2',
+	},
+	# expanded map: mountaineye1
+	6 => {
+		N => 'Mountain eye cave part 1',
+	},
+	# expanded map: mountaineye3
+	7 => {
+		N => 'Mountain eye cave part 3',
+	},
+	# expanded map: mountaineye20
+	8 => {
+		N => 'Mountain eye cave part 20',
+	},
+	# expanded map: mountaineye5
+	9 => {
+		N => 'Mountain eye cave part 5',
+	},
+	# expanded map: connection
+	10 => {
+		N => 'connection',
+	},
+	# expanded map: cob
+	11 => {
+		N => 'cob',
+	},
+);
+
+
+
+$PATTERN_DEFS = "./";
